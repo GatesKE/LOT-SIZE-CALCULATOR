7a2439c3b16b6f6635635847beebccb90f601be9
GatesKE <138989054+GatesKE@users.noreply.github.com>
2024-06-06 12:48:26 +0300

    views.py

--- Views.py
+++ Views.py
@@ -1,20 +1,30 @@
+
+from flask import Blueprint,render_template
+Calculator=Blueprint(__name__,"Calculator")
+
 def calculate_lot_size(risk_amount, commission_per_lot, sl_size):
+
     # Lot size calculation formula
     lot_size = risk_amount / ((10 * sl_size) + commission_per_lot)
+
     # Rounding the lot size to 2 decimal places
     lot_size = round(lot_size, 2)
     return lot_size
 
 def main():
+
     # Get user input
+
     risk_amount = float(input("Enter your risk amount in dollars: "))
     commission_per_lot = float(input("Enter your broker's commission per lot (in dollars): "))
     sl_size = float(input("Enter your SL size (in pips): "))
     
     # Calculate lot size
+
     lot_size = calculate_lot_size(risk_amount, commission_per_lot, sl_size)
     
     # Print the result
+    
     print(f"Calculated lot size: {lot_size}")
 
 if __name__ == "__main__":
